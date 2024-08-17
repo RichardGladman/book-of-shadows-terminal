cd500df34104a8fe3f34424f2896143ba85e0344
Richard Gladman <richard@thefifthcontinent.com>
2024-08-17 15:37:03 +0100

    Add polarity model

---
+++ model/polarity.cpp
@@ -0,0 +1,28 @@
+#include "polarity.h"
+
+Polarity::Polarity() {}
+
+std::string Polarity::get_name() const
+{
+    return name;
+}
+
+std::string Polarity::get_meaning() const
+{
+    return meaning;
+}
+
+void Polarity::set_name(std::string n)
+{
+    name = n;
+}
+
+void Polarity::set_meaning(std::string m)
+{
+    meaning = m;
+}
+
+bool Polarity::operator=(const Polarity &rhs) const
+{
+    return id == rhs.id && name == rhs.name && meaning == rhs.meaning;
+}
---
+++ model/polarity.h
@@ -0,0 +1,24 @@
+#ifndef POLARITY_H
+#define POLARITY_H
+
+#include <string>
+#include "baseentity.h"
+
+class Polarity : public BaseEntity
+{
+public:
+    Polarity();
+    std::string get_name() const;
+    std::string get_meaning() const;
+
+    void set_name(std::string n);
+    void set_meaning(std::string m);
+
+    bool operator=(const Polarity &rhs) const;
+
+private:
+    std::string name;
+    std::string meaning;
+};
+
+#endif // POLARITY_H
