ffa545552810ce735e8cd7dbe6ef08a73ce252bb
Richard Gladman <richard@thefifthcontinent.com>
2024-09-01 14:52:39 +0100

    Refactor Zodiac to use start and end days and months

--- include/model/zodiac.h
+++ include/model/zodiac.h
@@ -10,27 +10,31 @@ namespace Model
     class Zodiac : public BaseEntity
     {
     public:
-        Zodiac();
-        Zodiac(const std::string &name, const std::string &description,
-                const std::chrono::time_point<std::chrono::system_clock> &start_date, const std::chrono::time_point<std::chrono::system_clock> &end_date);
+        Zodiac(const std::string &name, const std::string &description, int start_day, int start_month, int end_day, int end_month);
         Zodiac(const Zodiac &source);
 
         std::string get_name() const;
-        std::chrono::time_point<std::chrono::system_clock> get_start_date() const;
-        std::chrono::time_point<std::chrono::system_clock> get_end_date() const;
+        int get_start_day() const;
+        int get_start_month() const;
+        int get_end_day() const;
+        int get_end_month() const;
         std::string get_description() const;
 
         void set_name(std::string name);
-        void set_start_date(std::chrono::time_point<std::chrono::system_clock> start_date);
-        void set_end_date(std::chrono::time_point<std::chrono::system_clock> end_date);
+        void set_start_day(int start_day);
+        void set_start_month(int start_month);
+        void set_end_day(int end_day);
+        void set_end_month(int end_month);
         void set_description(std::string description);
 
         bool operator==(const Zodiac &rhs) const;
 
     private:
         std::string m_name;
-        std::chrono::time_point<std::chrono::system_clock> m_start_date;
-        std::chrono::time_point<std::chrono::system_clock> m_end_date;
+        int m_start_day;
+        int m_start_month;
+        int m_end_day;
+        int m_end_month;
         std::string m_description;
     };
 }
--- src/model/zodiac.cpp
+++ src/model/zodiac.cpp
@@ -2,26 +2,33 @@
 
 namespace Model
 {
-    Zodiac::Zodiac() : Zodiac {"", "", std::chrono::time_point<std::chrono::system_clock> {}, std::chrono::time_point<std::chrono::system_clock> {}} {}
-    Zodiac::Zodiac(const std::string &name, const std::string &description,
-                const std::chrono::time_point<std::chrono::system_clock> &start_date,
-                const std::chrono::time_point<std::chrono::system_clock> &end_date) :
-                    m_name {name}, m_description {description}, m_start_date {start_date}, m_end_date {end_date} {}
-    Zodiac::Zodiac(const Zodiac &source) : Zodiac {source.m_name, source.m_description, source.m_start_date, source.m_end_date} {}
+    Zodiac::Zodiac(const std::string &name, const std::string &description, int start_day, int start_month, int end_day, int end_month) :
+    m_name {name}, m_description {description}, m_start_day {start_day}, m_start_month {start_month}, m_end_day {end_day}, m_end_month {end_month} {}
+    Zodiac::Zodiac(const Zodiac &source) : Zodiac {source.m_name, source.m_description, source.m_start_day, source.m_start_month, source.m_end_day, source.m_end_month} {}
 
     std::string Zodiac::get_name() const
     {
         return this->m_name;
     }
 
-    std::chrono::time_point<std::chrono::system_clock> Zodiac::get_start_date() const
+    int Zodiac::get_start_day() const
     {
-        return this->m_start_date;
+        return this->m_start_day;
     }
 
-    std::chrono::time_point<std::chrono::system_clock> Zodiac::get_end_date() const
+    int Zodiac::get_start_month() const
     {
-        return this->m_end_date;
+        return this->m_start_month;
+    }
+
+    int Zodiac::get_end_day() const
+    {
+        return this->m_end_day;
+    }
+
+    int Zodiac::get_end_month() const
+    {
+        return this->m_end_month;
     }
 
     std::string Zodiac::get_description() const
@@ -34,14 +41,24 @@ namespace Model
         this->m_name = name;
     }
 
-    void Zodiac::set_start_date(std::chrono::time_point<std::chrono::system_clock> start_date)
+    void Zodiac::set_start_day(int start_day)
     {
-        this->m_start_date = start_date;
+        this->m_start_day = start_day;
     }
 
-    void Zodiac::set_end_date(std::chrono::time_point<std::chrono::system_clock> end_date)
+    void Zodiac::set_start_month(int start_month)
     {
-        this->m_end_date = end_date;
+        this->m_start_month = start_month;
+    }
+
+    void Zodiac::set_end_day(int end_day)
+    {
+        this->m_end_day = end_day;
+    }
+
+    void Zodiac::set_end_month(int end_month)
+    {
+        this->m_end_month = end_month;
     }
 
     void Zodiac::set_description(std::string description)
@@ -51,7 +68,6 @@ namespace Model
 
     bool Zodiac::operator==(const Zodiac &rhs) const
     {
-        return this->id ==rhs.id && this->m_name == rhs.m_name && this->m_start_date == rhs.m_start_date &&
-                this->m_end_date == rhs.m_end_date && this->m_description == rhs.m_description;
+        return this->id ==rhs.id && this->m_name == rhs.m_name;
     }
 }
